dfc5d2445e40e867ae5258599dc99d41e3af08c5
prayna <44960226+prayna@users.noreply.github.com>
2019-01-06 18:13:10 +0900

    Update App.js
    API KEY 지움

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ import AppHeader from './components/AppHeader';
 import SearchBar from './components/SearchBar';
 import VideoList from './components/VideoList';
 
-const API_KEY = 'AIzaSyBU-6fLqU6uq4LKbKb7TzziPXbCJ5ULhoE';
+const API_KEY = 'YOUR_API_KEY';
 
 type Props = {};
 export default class App extends Component<Props> {
