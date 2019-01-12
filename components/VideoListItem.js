2966b3b10832e6c4ffa9456b3fb3aacaa17c0503
prayna <44960226+prayna@users.noreply.github.com>
2019-01-13 00:52:36 +0900

    Add files via upload

--- components/VideoListItem.js
+++ components/VideoListItem.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import { View, Text, Image }  from 'react-native';
+import { Card } from 'react-native-elements';
+import WatchButton from './WatchButton';
 
 const VideoListItem = ({ video }) => {
-    const { imageStyle } = styles;
+    const { 
+        cardStyle,
+        contentStyle,
+        titleStyle,
+        cannelTitleStyle,
+        descriptionStyle,
+        imageStyle
+     } = styles;
+
     const {
         title,
         channelTitle,
@@ -12,20 +22,56 @@ const VideoListItem = ({ video }) => {
 
     return (
         <View>
-            <Image 
-                style={imageStyle}
-                source={{ uri: url }}
-            />
-            <Text>{title}</Text>
-            <Text>{channelTitle}</Text>
-            <Text>{description}</Text>
+            <Card containerStyle={cardStyle}>
+                <Image 
+                    style={imageStyle}
+                    source={{ uri: url }}
+                />
+                <View style={contentStyle}>
+                    <Text style={titleStyle}>
+                        {title}
+                    </Text>
+
+                    <Text style={cannelTitleStyle}>
+                        {channelTitle}
+                    </Text>
+
+                    <Text style={descriptionStyle}>
+                        {description}
+                    </Text>
+
+                    <WatchButton videoId={video.id.videoId} />
+                </View>
+            </Card>
         </View>
     );
 };
 
 const styles = {
     imageStyle: {
-        alignSelf: 'stretch', height: 180
+        alignSelf: 'stretch',
+        height: 180
+    },
+    cardStyle: {
+        padding: 5
+    },
+    contentStyle: {
+        flex: 1,
+        padding: 5
+    },
+    titleStyle: {
+        fontSize: 12,
+        marginBottom: 5
+    },
+    cannelTitleStyle: {
+        fontSize: 11,
+        color: '#777',
+        marginBottom: 5,
+        alignSelf: 'flex-end'
+    },
+    descriptionStyle: {
+        fontSize: 11,
+        alignSelf: 'center'
     }
 };
 
